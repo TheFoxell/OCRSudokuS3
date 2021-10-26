d479d929d68157e5c00ea08841345e6c2e3e1de3
Axel COCHEPIN <axel.cochepin@epita.fr>
2021-10-27 01:32:07 +0200

    -lm makefile rot

--- rotation/rotation.h
+++ rotation/rotation.h
@@ -1,9 +1,14 @@
 #ifndef	ROTATION_H
 #define ROTATION_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <err.h>
 #include "SDL/SDL.h"
 #include "SDL/SDL_image.h"
+#include "pixel_operations.h"
 #include <math.h>
+#include <string.h>
 
 void menuRotation(char filename[]); 
 int present(char filename[]);
