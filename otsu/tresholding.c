a06c9a92f9064bbba741c8ea77cf6d72b83150f4
io.lart <io.lart@epita.fr>
2021-10-26 12:14:43 +0200

    rotation, black simple et debut otsu

---
+++ otsu/tresholding.c
@@ -0,0 +1,25 @@
+#include <err.h>
+#include "SDL/SDL.h"
+#include "SDL/SDL_image.h"
+#include "pixel_operations.h"
+
+int histogram(SDL_Surface *img)
+{
+  static Uint8 hist[256];
+
+  
+    for (int i = 0; i < img->w; i++)
+      {
+	for (int j = 0; j < img->h; j++)
+	  {
+	    Uint32 pixel = get_pixel(img, i, j);
+	    Uint8 color;
+	    SDL_GetRGB(pixel, img->format, &color, &color, &color);
+	    hist[color] += 1;
+	  }
+      }
+
+    return hist;
+}
+
+
