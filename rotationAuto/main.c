0798882bb1039ff3efbe0add766b7eca891a1fbd
fox <fox@kali>
2021-12-01 16:40:45 +0100

    reglage tools

---
+++ rotationAuto/main.c
@@ -0,0 +1,12 @@
+#include <string.h>                                                            
+#include <stdio.h>                                                             
+#include <err.h>
+#include "../GridCutout/gridcut.h"                                              
+#include "../GridCutout/griddetect.h"                                           
+#include "../rotation/rotation.h"
+
+
+int main()
+{
+  
+}
